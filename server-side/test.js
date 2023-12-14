3a192d6fffeaa6c408b9f3ea0bd6bb524ebcbb47
PoK3MOoN <petarsudar2@gmail.com>
2023-12-14 15:14:30 +0100

    Kolacici8

--- server-side/test.js
+++ server-side/test.js
@@ -20,7 +20,13 @@ const corsOptions = {
   origin: "https://najbolji-notes.netlify.app", // Zbog ovoga mi nije radio program, ne koristi localhost!
   credentials: true, //access-control-allow-credentials:true
   optionSuccessStatus: 200,
-  allowedHeaders: ["Authorization", "Cookie", "Cookies", "Set-Cookie"],
+  allowedHeaders: [
+    "Content-Type",
+    "Authorization",
+    "Cookie",
+    "Cookies",
+    "Set-Cookie",
+  ],
 };
 
 app.use(cors(corsOptions)); // Use this after the variable declaration
